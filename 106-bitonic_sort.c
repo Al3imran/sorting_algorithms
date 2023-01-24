368eee29b3b9c822f5141d605bbd45794c1ee4bd
Al3imran <111256161+Al3imran@users.noreply.github.com>
2023-01-24 22:13:58 +0300

    1

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,75 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * bitonic_sort - sorts an array following the Bitonic sort algorithm
- * @array: array of ints to sort
- * @size: size of the array
+ * print_sequence - prints an array
+ * @array: given arrat
+ * @low: lower boundary
+ * @high: upper boundary
+ */
+void print_sequence(int *array, int low, int high)
+{
+	char *sep;
+
+	for (sep = ""; low <= high; low++)
+	{
+		printf("%s%d", sep, array[low]);
+		sep = ", ";
+	}
+	printf("\n");
+}
+
+/**
+ * swap_dir - swaps tow elements if they are in the
+ * specified bitonic direction
+ * @array: given array
+ * @e1: first element
+ * @e2: second element
+ * @dir: direction 1 for UP and 0 for DOWN
+ */
+void swap_dir(int *array, int e1, int e2, int dir)
+{
+	int temp;
+
+	if (dir == (array[e1] > array[e2]))
+	{
+		temp = array[e1];
+		array[e1] = array[e2];
+		array[e2] = temp;
+	}
+}
+
+/**
+ * merge_sequence - merge sequence bitonically
+ * @array: given array
+ * @low: lower boundary
+ * @high: high boundary
+ * @dir: direction 1 for UP and 0 for DOWN
+ * @size: array size
+ */
+void merge_sequence(int *array, size_t low, size_t high, int dir, size_t size)
+{
+	size_t middle, i, j;
+
+	if (high - low < 1)
+		return;
+
+	middle = (low + high) / 2;
+	for (i = low, j = middle + 1; i <= middle; i++, j++)
+		swap_dir(array, i, j, dir);
+
+	merge_sequence(array, low, middle, dir, size);
+	merge_sequence(array, middle + 1, high, dir, size);
+}
+
+/**
+ * make_sequence - make bitonic sequence
+ * @array: given array
+ * @low: lower boundary
+ * @high: high boundary
+ * @dir: direction 1 for UP and 0 for DOWN
+ * @size: array size
+ */
+void make_sequence(int *array, size_t low, size_t high, int dir, size_t size)
+{
+	char *direction;
+	size_t middle;
+
+	if (high - low < 1)
+		return;
+
+	direction = dir ? "UP" : "DOWN";
+	printf("Merging [%ld/%ld] (%s):\n", high - low + 1, size, direction);
+	print_sequence(array, low, high);
+
+	middle = (low + high) / 2;
+	make_sequence(array, low, middle, 1, size);
+	make_sequence(array, middle + 1, high, 0, size);
+
+	merge_sequence(array, low, high, dir, size);
+	printf("Result [%ld/%ld] (%s):\n", high - low + 1, size, direction);
+	print_sequence(array, low, high);
+}
+
+/**
+ * bitonic_sort -  sorts an array of integers in
+ * ascending order using the Bitonic sort algorithm
+ * @array: given array
+ * @size: array size
  */
 void bitonic_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
 		return;
 
-	bitonic_recursion(array, 0, size - 1, 1, size);
-}
-
-/**
- * bitonic_recursion - recursive function for bitonic sort
- * @array: array to sort
- * @l: index of the left-most element
- * @r: index of the right-most element
- * @direction: ascending or descending
- * @size: size of the array
- */
-void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
-{
-	int step;
-
-	if (r - l >= 1)
-	{
-		step = (r + l) / 2;
-		printf("Merging [%d/%lu] ", r - l + 1, size);
-		if (direction)
-			printf("(UP):\n");
-		else
-			printf("(DOWN):\n");
-		print_array(array + l, r - l + 1);
-		bitonic_recursion(array, l, step, 1, size);
-		bitonic_recursion(array, step + 1, r, 0, size);
-		bitonic_merge(array, l, r, direction);
-		printf("Result [%d/%lu] ", r - l + 1, size);
-		if (direction)
-			printf("(UP):\n");
-		else
-			printf("(DOWN):\n");
-		print_array(array + l, r - l + 1);
-	}
-}
-
-/**
- * bitonic_merge - sorts and merges a sequence in ascending or descending order
- * @array: array to sort
- * @l: index of the left-most element
- * @r: index of the right-most element
- * @direction: ascending or descending
- */
-void bitonic_merge(int *array, int l, int r, int direction)
-{
-	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
-
-	if (r - l >= 1)
-	{
-		for (i = l; i < l + mid; i++)
-		{
-			if (direction == (array[i] > array[i + mid]))
-			{
-				tmp = array[i + mid];
-				array[i + mid] = array[i];
-				array[i] = tmp;
-			}
-		}
-		bitonic_merge(array, l, step, direction);
-		bitonic_merge(array, step + 1, r, direction);
-	}
-
+	make_sequence(array, 0, size - 1, 1, size);
 }
